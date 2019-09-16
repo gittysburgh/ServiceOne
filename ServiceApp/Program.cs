6fc0df48d0af3077d328b069a17fd3051a3a3744
Neville <neville.lewis@gmail.com>
2019-09-16 17:07:32 -0600

    added sub modules as reference to the main project

---
+++ ServiceApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
